67fab7e137fdc1eb34aa55809d09db9bd31e1525
Rahul Mahato <rahulmahatorvs8210@gmail.com>
2023-02-05 13:41:11 +0530

    project1

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("signup-button").addEventListener("click", function(event) {
+    event.preventDefault();
+    // Perform sign-up logic here
+  });
+  
+  document.getElementById("login-button").addEventListener("click", function(event) {
+    event.preventDefault();
+    // Perform login logic here
+  });
+  
